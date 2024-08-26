f0dc8c43779c5f4d503e78f23209cc6dbe8564c8
harish151 <125892721+harish151@users.noreply.github.com>
2024-08-26 11:28:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -225,11 +225,14 @@ def perform_task(search_query1, search_query2):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-            search_query1,search_query2 = request.form.get('search_query1'),request.form.get('search_query2')
-            data1,data2,suggest=productdata(search_query1,search_query2)
-            return render_template('project.html',data1=data1,data2=data2,suggest=suggest)
-    return render_template('project.html')
+    try:
+        if request.method == 'POST':
+                search_query1,search_query2 = request.form.get('search_query1'),request.form.get('search_query2')
+                data1,data2,suggest=productdata(search_query1,search_query2)
+                return render_template('project.html',data1=data1,data2=data2,suggest=suggest)
+        return render_template('project.html')
+    except:
+         return "<h1><font color='red'>CHECK YOUR INTERNET CONNECTION EITHER NOT CONNECTED OR SLOW.</font></h1>"
 
 if __name__=='__main__':
     app.run(debug=True)
